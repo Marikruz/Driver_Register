27be838bb928a615d1e0606310ca2e80a3a34931
zengbeibei_i <zengbeibei_i@didiglobal.com>
2021-01-25 10:33:59 +0800

    Initial Commit

---
+++ Driver_Register/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Driver_Register
+//
+//  Created by didi on 2021/1/25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Driver_Register/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Driver_Register
+//
+//  Created by didi on 2021/1/25.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
